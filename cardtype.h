ed8d90d79bced2f774b94bb0fd682e95915849f0
ruifeng-1 <55276267+ruifeng-1@users.noreply.github.com>
2019-10-29 19:42:48 +0800

    福建十三水
    优化运行时间，增加了转换json格式功能

--- cardtype.h
+++ cardtype.h
@@ -2,6 +2,9 @@
 #include<iostream>
 #include<vector>
 #include <algorithm>
+#include<string>
+#include"json/json.h"
+#include"include/curl/curl.h"
 using namespace std;
 
 bool cmp(const pair<char, int> &a, const pair<char, int> &b) //定义排序逻辑
@@ -13,245 +16,115 @@ bool cmp(const pair<char, int> &a, const pair<char, int> &b) //定义排序逻辑
 
 class SimpleCardtype//普通牌型
 {
-public:
-	int calculate_weights(vector<pair<char, int>> choose)//计算权值
+private:
+	int judge_cardtype()//判断牌型
 	{
-		sort(choose.begin(), choose.end(), cmp);//
+		if (poker.size() == 5)
+		{
+			if (sameColor && straight)
+				return 8;
+			if (type[4])
+				return 7;
+			if (type[3] && type[2])
+				return 6;
+			if (sameColor)
+				return 5;
+			if (straight)
+				return 4;
+			if (type[3])
+				return 3;
+			if (type[2] >= 2)
+				return 2;
+			if (type[2])
+				return 1;
+			return 0;
+		}
+		else
+		{
+			if (type[3])
+				return 3;
+			if (type[2])
+				return 1;
+			return 0;
+		}
+	}
+	bool checkSameColor() {
+		if (poker.size() != 5) return false;
+		char color = poker[0].first;
+		for (int i = 1; i < 5; i++)
+			if (poker[i].first != color)
+				return false;
+		return true;
+	}
+	bool checkStraight() {
+		if (poker.size() != 5) return false;
+		for (int i = 0; i < 4; i++)
+			if (poker[i].second != poker[i + 1].second - 1)
+				return false;
+		return true;
+	}
+public:
+	vector<pair<char, int> > poker;
+	vector<string > input;
+	int type[11];
+	bool sameColor, straight;
+	int weight;
+	SimpleCardtype() {}
+	SimpleCardtype(vector<pair<char, int> > &choose) {
+		init(choose);
+	}
+	void init(vector<pair<char, int> > &choose) {
+		swap(choose, poker);
+		fill(type, type + 11, 0);
+		sort(poker.begin(), poker.end(), cmp);
 		vector<pair<int, int> > tmp;
 		for (int i = 0; i <= 13; i++) tmp.push_back({ 0, i });
-		for (int i = 0; i < choose.size(); i++)
-			tmp[choose[i].second].first++;
+		for (int i = 0; i < poker.size(); i++)
+			tmp[poker[i].second].first++;
 		sort(tmp.begin(), tmp.end(), greater<pair<int, int> >());
 		int val = 0;
-		for (int i = 0; i < tmp.size() && tmp[i].first != 0; i++)
+		for (int i = 0; i < tmp.size() && tmp[i].first != 0; i++) {
 			val = 20 * val + tmp[i].second;
-		int k = 1e8;
-		int weight = judge_cardtype(choose)*k;
-		return weight + val;
-	}
-	int judge_cardtype(vector<pair<char, int>> choose)//判断牌型
-	{
-		if (choose.size() == 5)
-		{
-			if (cardtype1(choose) & cardtype2(choose))
-				return 8;
-			if (cardtype3(choose))
-				return 7;
-			if (cardtype4(choose) & cardtype5(choose))
-				return 6;
-			if (cardtype1(choose))
-				return 5;
-			if (cardtype2(choose))
-				return 4;
-			if (cardtype4(choose))
-				return 3;
-			if (cardtype6(choose))
-				return 2;
-			if (cardtype5(choose))
-				return 1;
-			return 0;
+			type[tmp[i].first] ++;
 		}
-		else
-		{
-			if (cardtype4(choose))
-				return 3;
-			if (cardtype5(choose))
-				return 1;
-			return 0;
-		}
-		
-	}
-	bool cardtype1(vector<pair<char, int>> choose)//同花
-	{
-		char color = choose[0].first;
-		for (int i = 1; i < 5; i++)
-		{
-			if (choose[i].first != color)
-				return false;
-		}
-		return true;
-	}
-	bool cardtype2(vector<pair<char, int>> choose)//顺子
-	{
-		for (int i = 0; i < 4; i++)
-		{
-			if (choose[i].second != choose[i + 1].second - 1)
-				return false;
-		}
-		return true;
-	}
-	bool cardtype3(vector<pair<char, int>> choose)//炸弹
-	{
-		for (int i = 0; i < 2; i++)
-		{
-			int ok = 1;
-			int number = choose[i].second;
-			for (int j = i + 1; j < i + 4; j++)
-			{
-				if (choose[j].second != number)
-				{
-					ok = 0;
-					break;
-				}
-			}
-			if (ok)
-				return true;
-		}
-		return false;
-	}
-	bool cardtype4(vector<pair<char, int>> choose)//三条
-	{
-		if (choose.size() == 5)
-		{
-			for (int i = 0; i < 3; i++)
-			{
-				int ok = 1;
-				int number = choose[i].second;
-				for (int j = i + 1; j < i + 3; j++)
-				{
-					if (choose[j].second != number)
-					{
-						ok = 0;
-						break;
-					}
-				}
-				if (ok)
-					return true;
-			}
-			return false;
-		}
-		else
-		{
-			for (int i = 1; i < 3; i++)
-			{
-				if (choose[i].second != choose[0].second)
-					return false;
-			}
-			return true;
-		}
-		
-	}
-	bool cardtype5(vector<pair<char, int>> choose)//对子
-	{
-		for (int i = 0; i < choose.size()-1; i++)
-		{
-			if (choose[i].second == choose[i + 1].second)
-				return true;
-		}
-		return false;
-	}
-	bool cardtype6(vector<pair<char, int>> choose)//两对
-	{
-		int ok = 0;
-		for (int i = 0; i < 4; i++)
-		{
-			if (choose[i].second == choose[i + 1].second)
-				ok++;
-		}
-		if (ok == 2)
-			return true;
-		else
-			return false;
+		if (poker.size() == 3) val *= 400;
+		sameColor = checkSameColor();
+		straight = checkStraight();
+		int k = 1e7;
+		weight = judge_cardtype()*k + val;
 	}
 };
 
-class SpecialCardtype//特殊牌型
+
+/*int write_data(char* buffer, size_t size, size_t nmemb, void* userp)//回调函数
 {
-public:
-	bool judge_cardtype(vector<pair<char, int>> group)//判断牌型
+	std::string * str = dynamic_cast<std::string *>((std::string *)userp);
+	str->append((char *)buffer, size * nmemb);
+	return nmemb;
+}
+
+void sign_in()
+{
+
+}
+
+void open(string token,string* response)
+{
+	curl_global_init(CURL_GLOBAL_ALL);
+	CURL* curl;
+	struct curl_slist *headers = NULL;
+	token = "X-Auth-Token:" + token;
+	headers = curl_slist_append(headers,token.c_str());
+	curl = curl_easy_init();
+	if (!curl)
 	{
-		sort(group.begin(), group.end(), cmp);//升序排序
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);//请求头
+		curl_easy_setopt(curl, CURLOPT_URL, "http://api.revth.com/game/open");//URL
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);//回调函数
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)response);//错误信息
+		int status_code = curl_easy_perform(curl);   // 执行
 	}
-	bool cardtype1(vector<pair<char, int>> group)//一条龙&至尊清龙
-	{
-		for (int i = 0; i < group.size(); i++)
-		{
-			if (group[i].second != group[i + 1].second - 1)
-				return false;
-		}
-		return true;
-	}
-	 bool cardtype2(vector<pair<char, int>> group)//十二皇族
-	 {
-		 int ok = 0;
-		 for (int i = 0; i < group.size(); i++)
-		 {
-			 if (group[i].second <= 9)
-				 ok ++;
-			 if (ok >= 2)
-				 return false;
-		 }
-		 return true;
-	 }
-	 bool cardtype3(vector<pair<char, int>> group)//三同花顺
-	 {
+}*/
 
-	 }
-	 bool cardtype4(vector<pair<char, int>> group)//三分天下
-	 {
-		 int ok = 0;
-		 for (int i = 0; i < group.size() - 3; i++)
-		 {
 
-		 }
-	 }
-	 bool cardtype5(vector<pair<char, int>> group)//全大
-	 {
-		 for (int i = 0; i < group.size(); i++)
-		 {
-			 if (group[i].second < 7)
-				 return false;
-		 }
-		 return true;
-	 }
-	 bool cardtype6(vector<pair<char, int>> group)//全小
-	 {
-		 for (int i = 0; i < group.size(); i++)
-		 {
-			 if (group[i].second > 7)
-				 return false;
-		 }
-		 return true;
-	 }
-	 bool cardtype7(vector<pair<char, int>> group)//凑一色
-	 {
-		 char color = group[0].first;
-		 if(color=='*'&&color=='#')//方块或梅花
-			 for (int i = 1; i < group.size(); i++)
-			 {
-				 if (group[i].first != '*'&&group[i].first != '#')
-					 return false;
-			 }
-		 else//红桃或黑桃
-			 for (int i = 1; i < group.size(); i++)
-			 {
-				 if (group[i].first != '&'&&group[i].first != '$')
-					 return false;
-			 }
-		 return true;
-	 }
-	 bool cardtype8(vector<pair<char, int>> group)//双怪冲三
-	 {
 
-	 }
-	 bool cardtype9(vector<pair<char, int>> group)//四套三条
-	 {
-
-	 }
-	 bool cardtype10(vector<pair<char, int>> group)//五对三条
-	 {
-
-	 }
-	 bool cardtype11(vector<pair<char, int>> group)//六对半
-	 {
-
-	 }
-	 bool cardtype12(vector<pair<char, int>> group)//三顺子
-	 {
-
-	 }
-	 bool cardtype13(vector<pair<char, int>> group)//三同花
-	 {
-
-	 }
-};
--- main.cpp
+++ main.cpp
@@ -1,114 +1,215 @@
 #include<stdio.h>
 #include<iostream>
 #include<vector>
+#include<string>
 #include"cardtype.h"
+#include"json/json.h"
+#include"include/curl/curl.h"
 using namespace std;
 typedef long long ll;
-
-pair<char, int> a[14];//全局变量，牌组
-vector<pair<char, int> > best1, best2, best3;//最优牌组
+pair<char, int> a[14] = { {'&',0 },{'&',2 },{'&',3 },{'&',4 },{'&',5 },{'&',6 }, {'*',13 } ,{'&',13 }, {'#',13 }, {'$',13 }, {'$',9 }, {'#',9 }, {'*',9 }, {'&',7} };//全局变量，牌组
+SimpleCardtype best1, best2, best3;//最优牌组
+Json::FastWriter writer;
+int ID;
 int max_weight = 0;//最大权值
-int i = 0;//枚举次数
+int i = 0, t1 = 0, t2 = 0;
 
 class Enume//枚举
 {
 public:
-	vector<pair<char, int> > choose1, choose2, choose3;//三墩
-	SimpleCardtype simple;
-	void dfs1(vector<int> p, int r, int n);
-	void dfs2(vector<int> p, int r, int n);
-	void dfs3(vector<int> p, int r, int n);
+    SimpleCardtype front, mid, back;
+	void dfs1(vector<int> &p, int r, int n);
+	void dfs2(vector<int> &p, int r, int n);
+	void dfs3(vector<int> &p, int r, int n);
+	vector<pair<char, int> > choose;
 	void best_choose();
-
 };
-void Enume::dfs1(vector<int> p, int r, int n) {
+
+void Enume::dfs1(vector<int> &p, int r, int n) {
 	for (ll s = (1 << r) - 1; s < 1 << n;) {
-		choose1.clear();
+        t1 = s;
+		choose.clear();
 		vector<int> cur = p;
 		for (int j = n - 1; j >= 0; j--) {
-			if (((s >> j) & 1)) choose1.push_back(a[p[j]]), cur.erase(cur.begin() + j, cur.begin() + j + 1);
+			if (((s >> j) & 1)) choose.push_back(a[p[j]]), cur.erase(cur.begin() + j, cur.begin() + j + 1);
 		}
-		dfs2(cur, 3, 8);
+		back.init(choose);
+		dfs2(cur, 5, 8);
 		ll x = s & -s;
 		ll y = s + x;
 		s = ((s&~y) / x >> 1) | y;
 	}
 }
 
-void Enume::dfs2(vector<int> p, int r, int n) {
+void Enume::dfs2(vector<int> &p, int r, int n) {
 	for (ll s = (1 << r) - 1; s < 1 << n;) {
-		choose3.clear();
+	    t2 = s;
+		choose.clear();
 		vector<int> cur = p;
 		for (int j = n - 1; j >= 0; j--) {
-			if (((s >> j) & 1)) choose3.push_back(a[p[j]]), cur.erase(cur.begin() + j, cur.begin() + j + 1);
+			if (((s >> j) & 1)) choose.push_back(a[p[j]]), cur.erase(cur.begin() + j, cur.begin() + j + 1);
 		}
-		dfs3(cur, 5, 5);
+		mid.init(choose);
 		ll x = s & -s;
 		ll y = s + x;
 		s = ((s&~y) / x >> 1) | y;
+		if(mid.weight > back.weight) continue;
+		dfs3(cur, 3, 3);
 	}
 }
 
-void Enume::dfs3(vector<int> p, int r, int n) {
-	choose2.clear();
+void Enume::dfs3(vector<int> &p, int r, int n) {
+	choose.clear();
 	for (int j = 0; j < n; j++) {
-		choose2.push_back(a[p[j]]);
+		choose.push_back(a[p[j]]);
 	}
-	//cout << ++i << endl;//输出枚举次数
+	front.init(choose);
+	if(front.weight > mid.weight) return ;
 	best_choose();
 }
 
 void Enume::best_choose()//权值
 {
-	int weight1 = simple.calculate_weights(choose1), 
-		weight2 = simple.calculate_weights(choose2), 
-		weight3 = simple.calculate_weights(choose3);
-	if (weight1 > weight2 && weight2 > weight3)
-	{
-		int weight = weight1 + weight2 + weight3;
-		if (weight > max_weight)
-		{
-			max_weight = weight;
-			best1 = choose1;
-			best2 = choose2;
-			best3 = choose3;//把三墩牌存进最优牌组
-		}
-	}
+    int weight = front.weight + mid.weight + back.weight;
+    if (weight > max_weight)
+    {
+        max_weight = weight;
+        best1 = front;
+        best2 = mid;
+        best3 = back;
+    }
 }
 
 void input_best()//出牌测试
 {
-	for (auto x : best3) {
+	for (auto x : best1.poker) {
 		cout << x.first << x.second << " ";
 	}
 	cout << endl;
-	for (auto x : best2) {
+	for (auto x : best2.poker) {
 		cout << x.first << x.second << " ";
 	}
 	cout << endl;
-	for (auto x : best1) {
+	for (auto x : best3.poker) {
 		cout << x.first << x.second << " ";
 	}
 	cout << endl;
+}
 
-	SimpleCardtype simple;
-	cout << simple.calculate_weights(best3) << endl;
-	cout << simple.calculate_weights(best2) << endl;
-	cout << simple.calculate_weights(best1) << endl;
-	cout << max_weight << endl;
+SimpleCardtype transform(SimpleCardtype best1)//转换
+{
+	for (int i = 0; i < best1.poker.size(); i++)
+	{
+		string str = "0";
+		str[0] = best1.poker[i].first;
+		if (best1.poker[i].second < 10)
+			best1.input.push_back(str + to_string(best1.poker[i].second));
+		else if (best1.poker[i].second == 11)
+			best1.input.push_back(str + "J");
+		else if (best1.poker[i].second == 12)
+			best1.input.push_back(str + "Q");
+		else if (best1.poker[i].second == 13)
+			best1.input.push_back(str + "K");
+		else if (best1.poker[i].second == 14) 
+			best1.input.push_back(str + "A");
+	}
+	return best1;
+}
+
+void input_best_2()
+{
+	best1 = transform(best1);
+	best2 = transform(best2);
+	best3 = transform(best3);
+	for (auto x : best1.input) {
+		cout << x << " ";
+	}
+	cout << endl;
+	for (auto x : best2.input) {
+		cout << x << " ";
+	}
+	cout << endl;
+	for (auto x : best3.input) {
+		cout << x << " ";
+	}
+	cout << endl;
+}
+
+Json::Value data_to_json(SimpleCardtype best1, SimpleCardtype best2, SimpleCardtype best3,int ID)
+{
+	string str;
+	Json::Value data;
+	data["id"] = Json::Value(ID);
+	str = best1.input[0];
+	for (int i = 1; i < best1.input.size(); i++)
+	{
+		str += " " + best1.input[i];
+	}
+	data["card"].append(str);
+	str = best2.input[0];
+	for (int i = 1; i < best2.input.size(); i++)
+	{
+		str += " " + best2.input[i];
+	}
+	data["card"].append(str);
+	str = best3.input[0];
+	for (int i = 1; i < best3.input.size(); i++)
+	{
+		str += " " + best3.input[i];
+	}
+	data["card"].append(str);
+	cout << writer.write(data) << endl;
+	return data;
+}
+
+void json_to_data(Json::Value data)
+{
+	string str;
+	str = writer.write(data["id"]);//解析ID
+	ID = atoi(str.c_str());//保存ID
+	str = writer.write(data["card"]);//解析牌组
+	int k = 1;
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == ' ')
+			break;
+		a[k].first = str[i++];
+		if (str[i] >= '2'&&str[i] <= '9')
+		{
+			if (str[i] != '1')
+				a[k++].second = str[i] - '0';
+			else
+			{
+				a[k++].second = 10;
+				i++;
+			}
+		}
+		else
+		{
+			if (str[i] == 'J')
+				a[k++].second = 11;
+			if (str[i] == 'Q')
+				a[k++].second = 12;
+			if (str[i] == 'K')
+				a[k++].second = 13;
+			if (str[i] == 'A')
+				a[k++].second = 14;
+		}
+	}
 }
 
 int main() {
 	vector<int> p;
 	Enume enume;
 	for (int i = 1; i <= 13; i++)
-		a[i] = { i % 4 + 'A', i }; // 字母花色， i牌大小
-	for (int i = 1; i <= 13; i++)
 		cout << a[i].first << a[i].second << " ";
 	cout << endl;
 	for (int i = 1; i <= 13; i++)
 		p.push_back(i);
 	enume.dfs1(p, 5, 13);
-	input_best();
+	input_best();//输出测试1
+	input_best_2();//输出测试2
+	Json::Value data = data_to_json(best1, best2, best3, 1001);
 	system("pause");
+    return 0;
 }
